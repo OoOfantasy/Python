a7ccf8585fdc2adbab0e45704471b837f08b585d
包子 <@DESKTOP-DKMPLFK>
2022-03-28 15:26:41 +0800

    2.变量和数据类型

---
+++ Python_Base/2_变量和数据类型.py
@@ -0,0 +1,27 @@
+# 变量：存储数据的容器
+# 变量命名规则：以字母数字下划线组成，不能以数字开头，区分大小写，不能是关键字
+'''
+    常用数据类型
+        int 整形
+        float 浮点型
+        bool 布尔值（True False）
+        str 字符串
+        dict 字典
+        tuple 元组
+        list 列表
+    type()方法可以用来获取变量的数据类型
+'''
+a = 10
+print(type(a))
+a = 'a'
+print(type(a))
+a = 1.5
+print(type(a))
+a = []
+print(type(a))
+a = {}
+print(type(a))
+a = ()
+print(type(a))
+a = True
+print(type(a))
